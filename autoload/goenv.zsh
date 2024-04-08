11b957ecc4acc54d935fe27ddead84df52b99042
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-04-08 19:01:26 +0800

    fix: bugs

--- autoload/goenv.zsh
+++ autoload/goenv.zsh
@@ -1,13 +1,14 @@
 # Try to find goenv, if it's not on the path
 export GOENV_ROOT="${GOENV_ROOT:=${HOME}/.goenv}"
-if ! type goenv > /dev/null && [ -f "${GOENV_ROOT}/bin/goenv" ]; then
+if ! type goenv >/dev/null && [ -f "${GOENV_ROOT}/bin/goenv" ]; then
     export PATH="${GOENV_ROOT}/bin:${PATH}"
 fi
 
 # Lazy load goenv
-if type goenv > /dev/null; then
-    export PATH="${GOENV_ROOT}/bin:${GOENV_ROOT}/shims:${PATH}"
+if type goenv >/dev/null; then
+    export PATH="${GOENV_ROOT}/shims:${PATH}"
     function goenv() {
+        # unset -f go
         unset -f goenv
         eval "$(command goenv init -)"
         goenv $@
--- autoload/pyenv.zsh
+++ autoload/pyenv.zsh
@@ -1,12 +1,12 @@
 # Try to find pyenv, if it's not on the path
 export PYENV_ROOT="${PYENV_ROOT:=${HOME}/.pyenv}"
-if ! type pyenv > /dev/null && [ -f "${PYENV_ROOT}/bin/pyenv" ]; then
+if ! type pyenv >/dev/null && [ -f "${PYENV_ROOT}/bin/pyenv" ]; then
     export PATH="${PYENV_ROOT}/bin:${PATH}"
 fi
 
 # Lazy load pyenv
-if type pyenv > /dev/null; then
-    export PATH="${PYENV_ROOT}/bin:${PYENV_ROOT}/shims:${PATH}"
+if type pyenv >/dev/null; then
+    export PATH="${PYENV_ROOT}/shims:${PATH}"
     function pyenv() {
         unset -f pyenv
         eval "$(command pyenv init -)"
